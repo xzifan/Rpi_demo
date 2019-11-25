a6e6014cc35759c5b0d7bb20f68434ea96ad573e
XCifer <13607960126@139.com>
2019-11-25 15:03:12 +0100

    222

--- test.py
+++ test.py
@@ -2,34 +2,47 @@ import os
 
 class DaoObject:
     
-    
-    
     def __init__(self,filepath):   
         with open(filepath,"r+") as file:
             list = file.readlines() 
         self.count = len(list)
+        self.filepath = filepath
         print("The size of db is: ",self.count)
         return
 
     def addItem(self, item):
-        file = open(filepath,"r+")
-        file.write("{}".format(item))
-        self.__init__(filepath)
+        filepath = self.filepath
+        file = open(filepath,"a")
+        file.write("{}\n".format(item))
         return
 
     def delItem(self, target):
+        filepath = self.filepath
         with open(filepath,"w") as f:
+            lines = f.readlines()
             for line in lines:
                 if line.strip("\n") != target:
                     f.write(line)
         return
 
     def updateItem(self, item, target):
-
+        filepath = self.filepath
+        with open(filepath,"w") as f:
+            lines = f.readlines()
+            for line in lines:
+                if line.strip("\n") != target:
+                    f.write(line)
+                else:
+                    f.write(item)
         return
 
     def queryItem(self, key):
-        
+        filepath = self.filepath
+        with open(filepath,"r") as f:
+            lines = f.readlines()
+            for i in range(len(lines)):
+                if(lines[i]==key):
+                    print ("This is No.{} of text.").format(i)
         return
 
 
