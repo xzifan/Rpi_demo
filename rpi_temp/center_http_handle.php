e92b7c0e9f5c7d76ad87d7a423ce32c19144dc32
XCifer <13607960126@139.com>
2019-12-13 11:23:58 +0100

    aa

--- rpi_temp/center_http_handle.php
+++ rpi_temp/center_http_handle.php
@@ -3,16 +3,18 @@
 
     $str = $_GET["str"];
 
-    // $url = 'http://users.du.se/~h19zifxi/ik2018distr/server_write_temp.php';
-    // $ch = curl_init($url);
+    $data = http_build_query(array(
+        'str' => $str
+    ));
 
-    // curl_setopt($ch, CURLOPT_POST, 1);
-    // curl_setopt($ch, CURLOPT_POSTFIELDS, $str);
-    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    // Create HTTP stream context
+    $context = stream_context_create(array(
+        'http' => array(
+            'method' => 'GET',
+        )
+    ));
 
-    // $response = curl_exec($ch);
-    // curl_close($ch);
-    $response = file_get_contents("http://users.du.se/~h19zifxi/ik2018distr/server_write_temp.php?str=$str")
-    echo $response
+    // Make GET request
+    $response = file_get_contents("http://users.du.se/~h19zifxi/ik2018distr/server_write_temp.php?str=$str", false, $context);
 
 ?>
